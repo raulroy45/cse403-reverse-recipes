3a86299a689269786cfb646114a22387bd34777c
Michael Hu <michaelkeminghu@gmail.com>
2022-04-25 21:48:34 -0700

    Create Android Studio project.

---
+++ app/app/src/main/java/com/cse403/reverserecipes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cse403.reverserecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
