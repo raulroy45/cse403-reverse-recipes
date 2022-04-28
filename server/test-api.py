500319aaf5055a6aaaffa04f78122ebf71ddad22
Ritadhwaj <rroy21@uw.edu>
2022-04-28 00:42:11 -0700

    added backend test files

--- server/test-api.py
+++ server/test-api.py
@@ -4,12 +4,12 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-class RecipeBook(Resource):
+class Recipes(Resource):
     def get(self):
         req = request.args.get("ingredients", type=str).split(",")
         return 200
 
-api.add_resource(RecipeBook, '/get-recipe')
+api.add_resource(Recipes, '/get-recipe')
 
 if __name__ == "__main__":
     app.run()
