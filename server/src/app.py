71353ed0361f9ffbb38275652e56bdec95487a6a
Ritadhwaj <rroy21@uw.edu>
2022-05-09 22:59:06 -0700

    Changed SQL Driver for pyodbc

--- server/src/app.py
+++ server/src/app.py
@@ -8,7 +8,7 @@ def setup_db():
     database = 'rrecipesDB'
     username = 'azureuser'
     password = 'xgD72UEc4GH5SLw'   
-    driver = '{ODBC Driver 18 for SQL Server}'
+    driver = '{FreeTDS}'
 
     db = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     return db
