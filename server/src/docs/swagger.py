61bd526cde5a103bb3a6e572bfc409dd0c76a612
Han Nguyen <han.nguyen1310@gmail.com>
2022-05-11 18:57:25 -0700

    Add API documentation with Swagger

---
+++ server/src/docs/swagger.py
@@ -0,0 +1,11 @@
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Reverse Recipes API",
+        "description": "API for Reverse Recipes",
+        "version": "1.0"
+    },
+    "schemes": [
+        "http"
+    ]
+}
