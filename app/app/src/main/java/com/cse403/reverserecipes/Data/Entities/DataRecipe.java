3096212e8a6043fbd65b5303eb64f237e5350dde
MichaelKHuUW <mkhu@cs.washington.edu>
2022-05-24 20:13:48 -0700

    Add Room table for saved recipes.

--- app/app/src/main/java/com/cse403/reverserecipes/Data/Entities/DataRecipe.java
+++ app/app/src/main/java/com/cse403/reverserecipes/Data/Entities/DataRecipe.java
@@ -1,16 +1,30 @@
 package com.cse403.reverserecipes.Data.Entities;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "saved_recipe_table")
 public class DataRecipe {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "rid")
     private final int mRid;
 
+    @ColumnInfo(name = "image")
     private final String mImage;
 
+    @ColumnInfo(name = "link")
     private final String mLink;
 
+    @ColumnInfo(name = "title")
     private final String mTitle;
 
+    @ColumnInfo(name = "total_time")
     private final int mTotalTime;
 
+    @ColumnInfo(name = "yields")
     private final int mYields;
 
     public DataRecipe(int rid, String image, String link, String title, int totalTime, int yields) {
